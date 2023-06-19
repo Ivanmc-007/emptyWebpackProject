629742916857131070218144333212c69b99ef34
Ivanmc-007 <ivanmc-007@mail.ru>
2023-06-19 11:03:19 +0300

    feat: add common files of the project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './index.html';
+import './index.scss';
+import "./modules/calc";
+// TODO: just for Example
+// import {mult, sum} from "./modules/calc";
+// import code from './img/Simpsons-family.jpg';
+
+// const img = new Image();
+// img.src = code;
+// img.width=300;
+// const imgWrap = document.querySelector('.footer');
+// imgWrap.append(img);
+// console.log(mult(3, 4));
+//
+// console.log(sum(3, 4));
+// sum(3, 4).then(aa => console.log(aa));
+console.log('***START***');
